5f6fe02a4811d12faa4d95010f389b69b542290b
Leavend <11201089@student.itk.ac.id>
2025-06-04 13:29:14 +0800

    refactor(menus): comment out settings menu

--- constants/menus.ts
+++ constants/menus.ts
@@ -35,38 +35,38 @@ export const navMenu: NavMenu[] = [
           },
         ],
       },
-      {
-        title: 'Settings',
-        icon: 'i-lucide-settings',
-        new: true,
-        children: [
-          {
-            title: 'Profile',
-            icon: 'i-lucide-circle',
-            link: '/settings/profile',
-          },
-          {
-            title: 'Account',
-            icon: 'i-lucide-circle',
-            link: '/settings/account',
-          },
-          {
-            title: 'Appearance',
-            icon: 'i-lucide-circle',
-            link: '/settings/appearance',
-          },
-          {
-            title: 'Notifications',
-            icon: 'i-lucide-circle',
-            link: '/settings/notifications',
-          },
-          {
-            title: 'Display',
-            icon: 'i-lucide-circle',
-            link: '/settings/display',
-          },
-        ],
-      },
+      // {
+      //   title: 'Settings',
+      //   icon: 'i-lucide-settings',
+      //   new: true,
+      //   children: [
+      //     {
+      //       title: 'Profile',
+      //       icon: 'i-lucide-circle',
+      //       link: '/settings/profile',
+      //     },
+      //     {
+      //       title: 'Account',
+      //       icon: 'i-lucide-circle',
+      //       link: '/settings/account',
+      //     },
+      //     {
+      //       title: 'Appearance',
+      //       icon: 'i-lucide-circle',
+      //       link: '/settings/appearance',
+      //     },
+      //     {
+      //       title: 'Notifications',
+      //       icon: 'i-lucide-circle',
+      //       link: '/settings/notifications',
+      //     },
+      //     {
+      //       title: 'Display',
+      //       icon: 'i-lucide-circle',
+      //       link: '/settings/display',
+      //     },
+      //   ],
+      // },
     ],
   },
 ]
